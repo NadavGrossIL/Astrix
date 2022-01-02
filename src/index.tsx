c8f6181f644b6f89c567c27128ae0de910f79cfd
NadavGrossIL <41901600+NadavGrossIL@users.noreply.github.com>
2022-01-02 23:41:06 +0200

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,17 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { store } from './app/store';
-import { Provider } from 'react-redux';
+import {store} from './app/store';
+import {Provider} from 'react-redux';
 import * as serviceWorker from './serviceWorker';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {SingleCharacter} from "./features/characters/SingleCharacter";
+import {CharactersList} from "./features/characters/CharactersList";
 
+export const ALL_CHARACTERS_PATH = "characters";
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Provider store={store}>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<App/>}>
+                        <Route path={ALL_CHARACTERS_PATH} element={<CharactersList/>}/>
+                        <Route path=":characterId" element={<SingleCharacter/>}/>
+                    </Route>
+                </Routes>
+            </BrowserRouter>
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
