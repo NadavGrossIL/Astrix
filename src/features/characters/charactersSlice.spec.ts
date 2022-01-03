9ec268b5428fba8c50e5b774be4ef2cfc1488a42
NadavGrossIL <41901600+NadavGrossIL@users.noreply.github.com>
2022-01-03 17:24:28 +0200

    Update charactersSlice.spec.ts

--- src/features/characters/charactersSlice.spec.ts
+++ src/features/characters/charactersSlice.spec.ts
@@ -5,14 +5,22 @@ describe('counter reducer', () => {
         loadedCharacters: [],
     };
     it('should handle initial state', () => {
-        expect(charactersReducer(undefined, { type: 'unknown' })).toEqual({
+        expect(charactersReducer(undefined, {type: 'unknown'})).toEqual({
             characters: [],
             info: {}
         });
     });
 
     it('should handle add characters', () => {
-        const actual = charactersReducer(initialState, addCharacters([{name: "rick", id: 1234, image:'someUrl'}]));
+        const actual = charactersReducer(initialState, addCharacters([{
+            name: "rick",
+            id: 1234,
+            image: 'someUrl',
+            status: 'alive',
+            species: "human",
+            gender: "male",
+            location: {name: "earth", url: "someUrl"}
+        }]));
         expect(actual.loadedCharacters).toEqual([{name: "rick"}]);
     });
 });
